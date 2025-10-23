3ad6c5375a5ddfb74940b37798a1abf585432d1c
João Vitor P.R. <jvprocha.05@gmail.com>
2025-10-23 11:34:54 -0300

    Implementando alterações

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,13 @@
-import MenuItem from "./MenuItem";
-
-const Menu = ((item) => {
+import MenuItem from './MenuItem'
+ 
+const Menu = ({items}) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-"></div>
+    <div className="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 gap-8">
+      {items.map((item)=>(
+        <MenuItem key={item} {...item}/>
+      ))}
+    </div>
   )
-})
+}
+ 
+export default Menu
