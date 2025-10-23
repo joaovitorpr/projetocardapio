dd56d8c494cb83317e0a4f5ddde897ebd52be03d
João Vitor P.R. <jvprocha.05@gmail.com>
2025-10-23 11:29:36 -0300

    7-estilizando o projeto com tailwindcss

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,7 @@
 import MenuItem from "./MenuItem";
 
-export const Menu = ({items}) => {
+const Menu = ((item) => {
   return (
-    <div>
-      {items.map((item)=>(
-        <MenuItem key={item} {...item}/>
-      ))}
-    </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-"></div>
   )
-}
-
-export default Menu;
+})
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,16 +1,15 @@
-//Destruct - vai acessar os dados atraves de outro componente
-export const MenuItem = ({titulo,preco,img,descricao}) => {
+//destruct - vai acessar os dados em outro local
+const MenuItem = ({titulo,preco,img,descricao}) => {
   return (
-    <article>
-        <img src={img} alt={titulo}/>
-        <div>
-            <header>
-                <h1>{titulo}</h1>
-                {/*toFixed pega duas casas depois da virgula*/}
-                <p>R$ {preco.toFixed(2)}</p>
-            </header>
-            <p>{descricao}</p>
-        </div>
+    <article className="bg-white rounded-xl shadow-lg flex flex-col overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-xl">
+      <img src={img} alt={titulo} className="w-full h-52 object-cover rounded-t-xl" />
+      <div className="p-5 flex flex-col">
+        <header className="flex justify-between items-baseline border-b-2 border-orange-200 pb-2 mb-4">
+          <h4 className="text-xl text-slate-800  font-medium">{titulo}</h4>
+          <p className="text-xl text-orange-500 font-bold">R$ {preco.toFixed(2)}</p>
+        </header>
+        <p className="text-base text-gray-700 ">{descricao}</p>
+      </div>
     </article>
   )
 }
