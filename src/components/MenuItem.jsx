37e4fc075353b8ec3702795ddc17ea8a842fe905
João Vitor P.R. <jvprocha.05@gmail.com>
2025-10-23 10:41:00 -0300

    4-codificando o componenete MenuItem

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,8 +1,16 @@
-
-
-export const MenuItem = () => {
+//Destruct - vai acessar os dados atraves de outro componente
+export const MenuItem = ({titulo,preco,img,descricao}) => {
   return (
-    <div>MenuItem</div>
+    <article>
+        <img src={img} alt={titulo}/>
+        <div>
+            <header>
+                <h1>{titulo}</h1>
+                {/*toFixed pega duas casas depois da virgula*/}
+                <p>R$ {preco.toFixed(2)}</p>
+            </header>
+        </div>
+    </article>
   )
 }
 
